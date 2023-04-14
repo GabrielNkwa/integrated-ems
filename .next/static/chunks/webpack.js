4d14f0dfca9b5c7acca1d30fd723c3d505ba4478
Gabriel Nkwa <gnkwa@outlook.com>
2023-04-14 13:48:24 +0100

    separated mobile component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b26a05eb9489e2e5"; }
+/******/ 		__webpack_require__.h = function() { return "79bbc51159dd27fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
