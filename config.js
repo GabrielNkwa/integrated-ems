e5d630c96433ad0163077f0b883a3e1df31c80f7
Gabriel Nkwa <gnkwa@outlook.com>
2023-04-27 14:52:08 +0100

    update

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChQrS4P0-a2-k6WlBFxfsJ-d0jIlhb3Uo",
+  authDomain: "integrated-ems-8b506.firebaseapp.com",
+  projectId: "integrated-ems-8b506",
+  storageBucket: "integrated-ems-8b506.appspot.com",
+  messagingSenderId: "233853266139",
+  appId: "1:233853266139:web:c625d0774836f12bff04fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
