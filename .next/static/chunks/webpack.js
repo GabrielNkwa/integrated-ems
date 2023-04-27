6d38f956c528d4cedb16eafa6fbb6cc3cf306e10
Gabriel Nkwa <gnkwa@outlook.com>
2023-04-27 03:26:19 +0100

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b223cd6e0bef5848"; }
+/******/ 		__webpack_require__.h = function() { return "83fecdbaae63a646"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
